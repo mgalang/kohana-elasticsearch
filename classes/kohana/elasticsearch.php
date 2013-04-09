efadde942081c7749c5cce1c79e2ce5b03514791
Marc Galang <marchenrygalang@gmail.com>
2013-04-09 13:36:28 +0300

    Changed factory to instance. Fix delete method. Added update method. Fix search method

--- classes/kohana/elasticsearch.php
+++ classes/kohana/elasticsearch.php
@@ -6,12 +6,18 @@
 class Kohana_Elasticsearch{
   public $host, $index, $port;
 
-  public static function factory(){
-    $config = Kohana::$config->load('elasticsearch');
-    return new Elasticsearch($config->get('host'), $config->get('port'), $config->get('index'));
+  private static $instance;
+
+  public static function instance(){
+    if(!self::$instance){
+      $config = Kohana::$config->load('elasticsearch');
+      return self::$instance = new self($config->get('host'), $config->get('port'), $config->get('index'));
+    } else {
+      return self::$instance;
+    }
   }
 
-  protected function __construct($host, $port, $index = 'kohana'){
+  private function __construct($host, $port, $index = 'kohana'){
     $this->index = $index;
     $this->host = $host;
     $this->port = $port;
@@ -21,17 +27,19 @@ class Kohana_Elasticsearch{
     // create url
     $url = $this->host .'/'. $this->index . '/' . $path;
 
-    if(is_array($content))
-      $data = json_encode($content);
-    else
-      $data = $content;
-
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_PORT, $this->port);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, strtoupper($method));
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+    if(!empty($content)){
+      $data = $content;
+      if(is_array($content))
+        $data = json_encode($content);
+
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    }
 
     $result = curl_exec($ch);
     curl_close($ch);
@@ -41,29 +49,33 @@ class Kohana_Elasticsearch{
   function count($type){
     return $this->fetch($type . '/_count', 'GET', '{ matchAll:{} }');
   }
- 
+
   function status(){
     return $this->fetch('_status');
   }
 
   function create(){
-     $this->fetch(NULL, 'PUT');
+    $this->fetch(NULL, 'PUT');
   }
 
   function add($type, $id, $data){
     return $this->fetch($type . '/' . $id, 'PUT', $data);
   }
 
-  function delete(){
-     $this->fetch(NULL, 'DELETE');
+  function delete($type, $id){
+    $this->fetch($type . '/' . $id, 'DELETE');
+  }
+
+  function update($type, $id, $data){
+    $this->fetch($type . '/' . $id . '/_update', 'POST', $data);
   }
 
   function mapping($type, $data){
     return $this->fetch($type . '/_mapping', 'PUT', $data);
   }
 
-  function search($type, $q){
-    return $this->fetch($type . '/_search?' . http_build_query(array('q' => $q)));
+  function search($type, $query = array()){
+    return $this->fetch($type . '/_search', 'GET', $query);
   }
 }
 
